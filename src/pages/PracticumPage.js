965c381a61fa2e1d012028de8ddecf2a4d6570b3
JL <jljuanillo@live.mcl.edu.ph>
2022-05-29 01:04:14 +0800

    Added nature of assignments in overview

--- src/pages/PracticumPage.js
+++ src/pages/PracticumPage.js
@@ -12,7 +12,7 @@ const PracticumPage = ({ title }) => {
         <h1 className="font-display text-4xl">Overview of Practicum</h1>
         <div className="mx-8 flex flex-col gap-8">
           {/* LinkedIn Learning */}
-          <div className="bg-suisei-gray/70 backdrop-blur-md rounded-xl ring-2 ring-suisei-yellow flex flex-col gap-8 p-8">
+          <div className="bg-suisei-gray/70 backdrop-blur-md ring-2 ring-suisei-yellow flex flex-col gap-8 p-8">
             <div className="flex flex-row gap-8">
               <figure className="flex shrink-0 justify-center items-center bg-white rounded-full w-48 h-48 px-6">
                 <img alt="Linkedin Learning logo" src={linkedin} />
@@ -233,6 +233,34 @@ const PracticumPage = ({ title }) => {
                     </ol>
                   </div>
                 </div>
+
+                {/* Nature of tasks */}
+                <div className="flex flex-col gap-2">
+                  <h2 className="text-3xl font-bold">Nature of assignments</h2>
+                  <p>
+                    The nature of assignments given in LinkedIn Learning can be
+                    identified by two tasks:
+                  </p>
+                  <h3 className="block font-bold text-xl">Chapter Quiz</h3>
+                  <p className="ml-4 indent-4">
+                    These are given after each chapter in a learning course. It
+                    contains a few questions regarding the contents of the
+                    chapter. Your answers are given in a multiple-choice type,
+                    and you can rewatch parts of the chapter in case you get a
+                    wrong answer. Quizzes can be taken multiple times.
+                  </p>
+                  <h3 className="block font-bold text-xl">
+                    Activity/Challenge
+                  </h3>
+                  <p className="ml-4 indent-4">
+                    These are sometimes given in between chapters that pertain
+                    to certain hands-on activities such as coding exercises that
+                    applies the learnings covered in within the chapter.
+                    Usually, exercise files are given on the main page of the
+                    learning course and can be used to answer
+                    activities/challenges.
+                  </p>
+                </div>
               </div>
             </div>
 
@@ -265,7 +293,7 @@ const PracticumPage = ({ title }) => {
           </div>
 
           {/* Accenture */}
-          <div className="bg-suisei-gray/70 backdrop-blur-md rounded-xl ring-2 ring-suisei-yellow flex flex-col gap-8 p-8">
+          <div className="bg-suisei-gray/70 backdrop-blur-md ring-2 ring-suisei-yellow flex flex-col gap-8 p-8">
             <div className="flex flex-row gap-8">
               <figure className=" flex shrink-0 justify-center items-center bg-white rounded-full w-48 h-48 px-6">
                 <img alt="Accenture logo" src={accenture} />
@@ -399,6 +427,51 @@ const PracticumPage = ({ title }) => {
                     </div>
                   </a>
                 </div>
+
+                {/* Nature of tasks */}
+                <div className="mt-2 flex flex-col gap-2">
+                  <h2 className="text-3xl font-bold">Nature of assignments</h2>
+                  <p>
+                    The nature of assignments given in AWS Educate can be
+                    identified by the following tasks:
+                  </p>
+                  <h3 className="block font-bold text-xl">Hands-on practice</h3>
+                  <p className="ml-4 indent-4">
+                    These are interactive activities that are embedded in the
+                    modules that simulate how to navigate through AWS or to get
+                    more information on a certain topic.
+                  </p>
+                  <h3 className="block font-bold text-xl">Labs</h3>
+                  <p className="ml-4 indent-4">
+                    These are actual laboratory exercises that launches a
+                    temporary instance of an AWS account to experience an actual
+                    deployment environment. There are given step-by-step
+                    instructions that are required to get the final output to be
+                    graded based on the accuracy of steps followed.
+                  </p>
+                  <h3 className="block font-bold text-xl">Knowledge Check</h3>
+                  <p className="ml-4 indent-4">
+                    These are quizzes given after each module. They usually
+                    contain 5 randomized questions about each module. Quizzes
+                    can be taken multiple times and the highest score is
+                    recorded.
+                  </p>
+                  <h3 className="block font-bold text-xl">Final Assessment</h3>
+                  <p className="ml-4 indent-4">
+                    These are final quizzes given at the end of the course. They
+                    contain randomized questions from all modules. Final
+                    Assessment can be taken multiple times and the highest score
+                    is recorded.
+                  </p>
+                  <h3 className="block font-bold text-xl">Survey</h3>
+                  <p className="ml-4 indent-4">
+                    Two surveys are given at the start of the course and at the
+                    end. These usually ask on the quality of learning the course
+                    provides, any suggestions regarding the course, how likely
+                    you are to recommend AWS to a relative, and if you are
+                    interested in taking an AWS Certification.
+                  </p>
+                </div>
               </div>
             </div>
 
@@ -428,6 +501,14 @@ const PracticumPage = ({ title }) => {
                 </div>
               </div>
             </div>
+          </div>
+
+          {/* Total Hours */}
+          <div className="bg-suisei-gray/70 backdrop-blur-md ring-2 ring-suisei-yellow flex flex-col gap-2 p-8 items-center">
+            <h2 className="text-4xl font-bold font-display text-suisei-yellow">
+              Total Practicum Credited Hours
+            </h2>
+            <h2 className="text-6xl font-bold font-display">492 Hours</h2>
           </div>
         </div>
       </div>
