9f88417deb7d3088bc5e9ccc089121d648c6e73a
JL <jljuanillo@live.mcl.edu.ph>
2022-06-02 14:40:12 +0800

    Completed content of Practicum Appendix page

--- src/pages/PracticumAppendixPage.js
+++ src/pages/PracticumAppendixPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Resume from "../files/JohnLloydJuanillo_Resume.pdf";
+import LearningPath from "../files/JohnLloydJuanillo_LearningPath.pdf";
+import WeeklyJournal from "../files/JohnLloydJuanillo_WeeklyJournal.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
@@ -16,7 +18,7 @@ const PracticumAppendixPage = ({ title }) => {
             <h2 className="text-3xl font-bold">Curriculum Vitae (CV)</h2>
             <details>
               <summary className="cursor-pointer select-none">
-                Click to View CV
+                View Curriculum Vitae
               </summary>
               <section className="flex justify-center items-center">
                 <div className="h-[70rem] w-[53rem]">
@@ -32,10 +34,78 @@ const PracticumAppendixPage = ({ title }) => {
               </section>
             </details>
             <div className="flex">
-              <button className="bg-suisei-yellow rounded-xl px-5 py-3 pt-4 font-bold text-xl hover:bg-white hover:text-suisei-yellow hover:shadow-lg transition ease-in-out hover:scale-105 duration-300">
+              <a
+                href={Resume}
+                download
+                className="bg-suisei-yellow rounded-xl px-5 py-3 pt-4 font-bold text-xl hover:bg-white hover:text-suisei-yellow hover:shadow-lg transition ease-in-out hover:scale-105 duration-300"
+              >
                 <FontAwesomeIcon icon={solid("download")} />
                 <span className="ml-2">Download</span>
-              </button>
+              </a>
+            </div>
+          </div>
+
+          <div className="bg-suisei-gray/70 backdrop-blur-md ring-2 ring-suisei-yellow flex flex-col gap-4 p-8 text-justify">
+            <h2 className="text-3xl font-bold">
+              Practicum Learning Module Path Proposal
+            </h2>
+            <details>
+              <summary className="cursor-pointer select-none">
+                View Practicum Learning Module Path Proposal
+              </summary>
+              <section className="flex justify-center items-center">
+                <div className="h-[70rem] w-[53rem]">
+                  <object
+                    data={LearningPath}
+                    type="application/pdf"
+                    title="Test"
+                    className="h-full w-full"
+                  >
+                    <div>No online PDF viewer installed</div>
+                  </object>
+                </div>
+              </section>
+            </details>
+            <div className="flex">
+              <a
+                href={LearningPath}
+                download
+                className="bg-suisei-yellow rounded-xl px-5 py-3 pt-4 font-bold text-xl hover:bg-white hover:text-suisei-yellow hover:shadow-lg transition ease-in-out hover:scale-105 duration-300"
+              >
+                <FontAwesomeIcon icon={solid("download")} />
+                <span className="ml-2">Download</span>
+              </a>
+            </div>
+          </div>
+
+          <div className="bg-suisei-gray/70 backdrop-blur-md ring-2 ring-suisei-yellow flex flex-col gap-4 p-8 text-justify">
+            <h2 className="text-3xl font-bold">Complete Weekly Journal</h2>
+            <details>
+              <summary className="cursor-pointer select-none">
+                View Weekly Journal
+              </summary>
+              <section className="flex justify-center items-center">
+                <div className="h-[70rem] w-full">
+                  <object
+                    data={WeeklyJournal}
+                    type="application/pdf"
+                    title="Test"
+                    className="h-full w-full"
+                  >
+                    <div>No online PDF viewer installed</div>
+                  </object>
+                </div>
+              </section>
+            </details>
+            <div className="flex">
+              <a
+                href={WeeklyJournal}
+                download
+                className="bg-suisei-yellow rounded-xl px-5 py-3 pt-4 font-bold text-xl hover:bg-white hover:text-suisei-yellow hover:shadow-lg transition ease-in-out hover:scale-105 duration-300"
+              >
+                <FontAwesomeIcon icon={solid("download")} />
+                <span className="ml-2">Download</span>
+              </a>
             </div>
           </div>
 
