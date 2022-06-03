abf67965db4171a6b822955fc833364fa6892853
JL <jljuanillo@live.mcl.edu.ph>
2022-06-03 15:07:43 +0800

    Added practicum acceptance pdf to appendices page

--- src/pages/PracticumAppendixPage.js
+++ src/pages/PracticumAppendixPage.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Resume from "../files/JohnLloydJuanillo_Resume.pdf";
 import LearningPath from "../files/JohnLloydJuanillo_LearningPath.pdf";
 import WeeklyJournal from "../files/JohnLloydJuanillo_WeeklyJournal.pdf";
+import Acceptance from "../files/JohnLloydJuanillo_PracticumAcceptance.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
@@ -40,6 +41,39 @@ const PracticumAppendixPage = ({ title }) => {
             <div className="flex">
               <a
                 href={Resume}
+                download
+                className="bg-suisei-yellow rounded-xl px-5 py-3 pt-4 font-bold text-xl hover:bg-white hover:text-suisei-yellow hover:shadow-lg transition ease-in-out hover:scale-105 duration-300"
+              >
+                <FontAwesomeIcon icon={solid("download")} />
+                <span className="ml-2">Download</span>
+              </a>
+            </div>
+          </div>
+
+          <div className="bg-suisei-gray/70 backdrop-blur-md ring-2 ring-suisei-yellow flex flex-col gap-4 p-8 ">
+            <h2 className="text-2xl lg:text-3xl font-bold">
+              Practicum Acceptance
+            </h2>
+            <details>
+              <summary className="cursor-pointer select-none text-sm lg:text-base">
+                View Practicum Acceptance
+              </summary>
+              <section className="flex justify-center items-center">
+                <div className="h-[70rem] w-[53rem]">
+                  <object
+                    data={Acceptance}
+                    type="application/pdf"
+                    title="Acceptance"
+                    className="h-full w-full"
+                  >
+                    <div>No online PDF viewer installed</div>
+                  </object>
+                </div>
+              </section>
+            </details>
+            <div className="flex">
+              <a
+                href={Acceptance}
                 download
                 className="bg-suisei-yellow rounded-xl px-5 py-3 pt-4 font-bold text-xl hover:bg-white hover:text-suisei-yellow hover:shadow-lg transition ease-in-out hover:scale-105 duration-300"
               >
